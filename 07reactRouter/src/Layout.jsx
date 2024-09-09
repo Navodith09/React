5d371582fff9aff37c5184eb66cdcad714b8a3a6
Navodith09 <navodith.saptarshi@gmail.com>
2024-09-09 21:19:36 +0530

    improved the components

--- 07reactRouter/src/Layout.jsx
+++ 07reactRouter/src/Layout.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import Header from './components/Header/Header'
-import Home from './components/Home/Home'
 import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 
 function Layout() {
   return (
-    <>
-    <Header />
-    <Outlet />
-    <Footer />
-    </>
+
+    // To keep the Footer always at the bottom of the window even if the contents of the page is less.
+
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+
+    // <>
+    // <Header />
+    // <Outlet />
+    // <Footer />
+    // </>
   )
 }
 
--- 07reactRouter/src/components/Footer/Footer.jsx
+++ 07reactRouter/src/components/Footer/Footer.jsx
@@ -36,7 +36,7 @@ export default function Footer() {
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <a
-                                        href="https://github.com/hiteshchoudhary"
+                                        href="https://github.com/Navodith09"
                                         className="hover:underline"
                                         target="_blank"
                                         rel="noreferrer"
@@ -45,9 +45,14 @@ export default function Footer() {
                                     </a>
                                 </li>
                                 <li>
-                                    <Link to="/" className="hover:underline">
+                                <a
+                                        href="https://discord.gg/gQRqNf5M"
+                                        className="hover:underline"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
                                         Discord
-                                    </Link>
+                                    </a>
                                 </li>
                             </ul>
                         </div>
@@ -68,12 +73,14 @@ export default function Footer() {
                         </div>
                     </div>
                 </div>
-                <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+            </div>
+            <hr className="border-gray-200 w-full" />
+            <div className="mx-auto w-full max-w-screen-xl p-4">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
-                        © 2023
+                        © 2024
                         <a href="https://hiteshchoudhary.com/" className="hover:underline">
-                            hiteshchoudhary
+                            navodithmondal
                         </a>
                         . All Rights Reserved.
                     </span>
