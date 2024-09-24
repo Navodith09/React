98ffcdf059982d36560722e36f900875733d3c7a
Navodith09 <navodith.saptarshi@gmail.com>
2024-09-25 00:10:03 +0530

    completed todo with redux

--- reactRedux/src/App.jsx
+++ reactRedux/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
 
 
   return (
     <>
-    <h1>Hello React Redux</h1>
     <AddTodo />
     <Todos />
     </>
