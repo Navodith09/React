dd7d1a7ece02bbc7499686c16e9a4da2904b0a41
Navodith09 <navodith.saptarshi@gmail.com>
2024-09-24 19:52:58 +0530

    created components for redux toolkit

--- myPortfolio/src/App.jsx
+++ myPortfolio/src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1 className='m-3 p-4 text-center text-3xl rounded-md text-white bg-[#212121]'>Navodith Mondal</h1>
+      
     </>
   )
 }
--- myPortfolio/src/main.jsx
+++ myPortfolio/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from '../../07reactRouter/src/App.jsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [],
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider rouer={router}/>
   </StrictMode>,
 )
